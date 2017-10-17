8ee2001979f312dec330661cc31f6926c7f6204b
ryan sparks <rspa9428@gmail.com>
2017-10-16 21:48:39 -0400

    clean floyd w

--- cs231nq/floydwarshall.cu
+++ cs231nq/floydwarshall.cu
@@ -3,43 +3,13 @@
 #include<unistd.h>
 #include"k.h"
 #include<math.h>
-
-// CUDA Headers
 #include </usr/local/cuda/include/cuda.h>
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
-// Helper definition
-#define VAR(v, i) __typeof(i) v=(i)
-#define FOR(i, j, k) for (int i = (j); i <= (k); ++i)
-#define REP(i, n) for(int i = 0;i <(n); ++i)
-
-// CONSTS
-#define INF     1061109567 // 3F 3F 3F 3F
-#define CHARINF 63       // 3F    
-#define CHARBIT 8
-#define NONE    -1
 
 #define CMCPYHTD cudaMemcpyHostToDevice
 #define CMCPYDTH cudaMemcpyDeviceToHost
 
 // CONSTS for compute capability 2.0
 #define BLOCK_WIDTH 16
-#define WARP         32
-
-/** Cuda handle error, if err is not success print error and line in code
-*
-* @param status CUDA Error types
-*/
-#define HANDLE_ERROR(err) \
-{ \
-    if (err != cudaSuccess) \
-    { \
-        fprintf(stderr, "%s failed  at line %d \nError message: %s \n", \
-            __FILE__, __LINE__ ,cudaGetErrorString(err)); \
-        exit(EXIT_FAILURE); \
-    } \
-}
 
 extern  "C" K gpu_floydwarshall(K matrix);
 
@@ -49,7 +19,7 @@ extern  "C" K gpu_floydwarshall(K matrix);
 */
 __global__ void wake_gpu_kernel(int reps) 
 {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    I idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx >= reps) return;
 }
 
@@ -61,13 +31,13 @@ __global__ void wake_gpu_kernel(int reps)
 */
 __global__ void fw_kernel(const unsigned int u, const unsigned int n, int * const d)
 {
-    int v1 = blockDim.y * blockIdx.y + threadIdx.y;
-    int v2 = blockDim.x * blockIdx.x + threadIdx.x;
+    I v1 = blockDim.y * blockIdx.y + threadIdx.y;
+    I v2 = blockDim.x * blockIdx.x + threadIdx.x;
     
     if (v1 < n && v2 < n) 
     {
-        int newPath = d[v1 * n + u] + d[u * n + v2];
-        int oldPath = d[v1 * n + v2];
+        I newPath = d[v1 * n + u] + d[u * n + v2];
+        I oldPath = d[v1 * n + v2];
         if (oldPath > newPath)
         {
             d[v1 * n + v2] = newPath;
@@ -81,60 +51,51 @@ K gpu_floydwarshall(K matrix)
     unsigned int n = V;
     // Alloc host data for G - graph, d - matrix of shortest paths
     unsigned int size = V * V;
-    int *d = (int *) malloc (sizeof(int) * size);
-    int *dev_d = 0;
-    cudaError_t cudaStatus;
+    I *d = (int *) malloc (sizeof(int) * size);
+    I *dev_d = 0;
     cudaStream_t cpyStream;
 
     // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    HANDLE_ERROR(cudaStatus);
+    cudaSetDevice(0);
 
     // Initialize the grid and block dimensions here
     dim3 dimGrid((n - 1) / BLOCK_WIDTH + 1, (n - 1) / BLOCK_WIDTH + 1, 1);
     dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH, 1);
 
     // Create new stream to copy data
-    cudaStatus = cudaStreamCreate(&cpyStream);
-    HANDLE_ERROR(cudaStatus);
+    cudaStreamCreate(&cpyStream);
 
     // Allocate GPU buffers for matrix of shortest paths d)
-    cudaStatus =  cudaMalloc((void**)&dev_d, n * n * sizeof(int));
-    HANDLE_ERROR(cudaStatus);
+    cudaMalloc((void**)&dev_d, n * n * sizeof(int));
  
     // Wake up gpu
     wake_gpu_kernel<<<1, dimBlock>>>(32);
 
     // Copy input from host memory to GPU buffers.
-    int *host_memoryd = (int*)&(kI(matrix)[0]);
-    cudaStatus = cudaMemcpyAsync(dev_d, host_memoryd, n * n * sizeof(int), CMCPYHTD, cpyStream);
+    I *host_memoryd = (int*)&(kI(matrix)[0]);
+    cudaMemcpyAsync(dev_d, host_memoryd, n * n * sizeof(int), CMCPYHTD, cpyStream);
 
     // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    cudaStatus = cudaDeviceSynchronize();
-    HANDLE_ERROR(cudaStatus);
+    cudaDeviceSynchronize();
 
     cudaFuncSetCacheConfig(fw_kernel, cudaFuncCachePreferL1 );
-    FOR(u, 0, n - 1)
+    for (int u = 0; u <= (n-1); ++u)
     {
         fw_kernel<<<dimGrid, dimBlock>>>(u, n, dev_d);
     }
 
     // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    HANDLE_ERROR(cudaStatus);
+    cudaGetLastError();
 
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    HANDLE_ERROR(cudaStatus);
+    // copy mem from gpu back to host
+    cudaMemcpy(host_memoryd, dev_d, n * n * sizeof(int), CMCPYDTH);
 
-    cudaStatus = cudaMemcpy(host_memoryd, dev_d, n * n * sizeof(int), CMCPYDTH);
-    HANDLE_ERROR(cudaStatus);
+    // free memory on gpu
+    cudaFree(dev_d);
 
-    cudaStatus = cudaFree(dev_d);
-    HANDLE_ERROR(cudaStatus);
-
-    // Delete allocated memory 
+    // Delete allocated memory on host
     free(d);
+
     R r1(matrix);
 }
+
