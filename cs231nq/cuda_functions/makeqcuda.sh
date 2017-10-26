ed07ee004ec73de008e3f3352676bad5e16655f0
ryan sparks <rspa9428@gmail.com>
2017-10-25 20:41:28 -0400

    chagned cuda script comment

--- cs231nq/cuda_functions/makeqcuda.sh
+++ cs231nq/cuda_functions/makeqcuda.sh
@@ -1,2 +1,3 @@
 echo "making .so file for $1"
+# note for matrix multiply/cuBLAS stuff, need to also have "-lcurand -lcublas" after "--shared"
 nvcc --ptxas-options=-v --compiler-options '-fPIC -DKXVER=3' -o $QHOME/l64/$1.so --shared $1.cu
